bb531d1ba8545eb714142d40af100e86243d0646
Bernardo Amaral <bernardoamaral@cpejr.com.br>
2024-11-29 11:48:13 -0300

    commit

--- src/routes.js
+++ src/routes.js
@@ -6,21 +6,21 @@ const UsuarioValidator = require("./Validators/UsuarioValidator") ;
 const SessoesValidator = require("./Validators/SessoesValidator") ;
 const AuthValidator = require("./Validators/AuthValidator") ;
 const verificarJwt =require("./Middlewares/verificarJwt")
-
+const verificarUsuario = require("./Middlewares/verificarUsuario")
 
 
 const rotas = Router();
 
 //Sessoes
-rotas.post('/sessao' ,SessoesValidator.create , SessoesController.create) ;
-rotas.get('/sessao', SessoesController.read) ;
+rotas.post('/sessao' ,verificarJwt , verificarUsuario ,SessoesValidator.create , SessoesController.create) ;
+rotas.get('/sessao',verificarJwt , verificarUsuario , SessoesController.read) ;
 rotas.delete('/sessao/:id_usuario', SessoesController.delete)
 
 //Usuarios
 rotas.post('/usuarios' , UsuarioValidator.create ,UsuarioController.create) ;
 rotas.get('/usuarios', verificarJwt , UsuarioController.read) ;
-rotas.delete('/usuarios/:id', verificarJwt,UsuarioValidator.destroy , UsuarioController.delete); 
-rotas.put('/usuarios/:id',UsuarioValidator.update ,UsuarioController.update )
+rotas.delete('/usuarios/:id', verificarJwt, verificarUsuario ,UsuarioValidator.destroy , UsuarioController.delete); 
+rotas.put('/usuarios/:id',UsuarioValidator.update, verificarUsuario ,UsuarioController.update )
 
 //Auth
 rotas.post("/login" , AuthValidator.login ,AuthController.login) ;
